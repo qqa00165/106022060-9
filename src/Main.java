72e8f24e14a0f85fac2f06f29636caca281c5b62
qqa00165 <qqa00165@gmail.com>
2017-09-30 14:47:10 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int 國文,英文,數學;
+        國文=in.nextInt();
+        英文=in.nextInt();
+        數學=in.nextInt();
+        System.out.println(國文+英文+數學);
+        System.out.println((國文+英文+數學)/3);
+	// write your code here
+    }
+}
